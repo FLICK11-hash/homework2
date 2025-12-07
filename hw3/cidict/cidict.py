615c9a692d273ccb9db04e21d9546b5f39764a33
Conrad Flick <flick104@umn.edu>
2025-12-07 14:22:00 -0600

    Update cidict.py

--- hw3/cidict/cidict.py
+++ hw3/cidict/cidict.py
@@ -1,1 +1,29 @@
 # write your CIDict here
+# Used generative AI to get back into the flow of the definitions and structures of classes
+
+class CIDict(dict):     # Subclass of dict
+    def __init__(self, **kwargs):
+        dict.__init__(self)
+        for key, value in kwargs.items():       # Initializing all values
+            self[key] = value
+
+    def __setitem__(self, key, value):
+        if(isinstance(key, str)):       # Lower case sensitive
+            key = key.lower()
+        dict.__setitem__(self, key, value)
+        
+    def __getitem__(self, key):
+        if(isinstance(key, str)):       # Lower case sensitive
+            key = key.lower()
+        return dict.__getitem__(self, key)
+    
+    def __delitem__(self, key):
+        if(isinstance(key, str)):       # Lower case sensitive
+            key = key.lower()
+        dict.__delitem__(self, key)
+
+    def  update_all(self, func):
+        for key in list(self.keys()):       # Grabs all the old values, run it through the given function and then changes the values accordingly
+            old_value = self[key]
+            new_value = func(old_value)
+            self[key] = new_value
